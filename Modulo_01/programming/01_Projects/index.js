f7cbade0f614b3dc1f4369ca666c8da5ec5d5fa7
jord-99 <84523821+jord-99@users.noreply.github.com>
2021-06-24 11:21:49 -0500

    query

--- Modulo_01/programming/01_Projects/index.js
+++ Modulo_01/programming/01_Projects/index.js
@@ -4,8 +4,22 @@ const express = require('express'); //requiero el paquete express
 const app = express()
 //endpoint
 app.get('/', (req, res) => {
-    res.send("HOLA MUNDO, con ExpressJS")
+    res.send("HOLA MUNDO, con express")
 })
+
+app.get("/saludo", (req, res) => {
+    //a las query acedemos con req.query
+    //console.log(req.query);
+   const { 
+        query : { nombre, apellido },
+     } = req; 
+ /* const nombre = req.query.nombre
+ const apellido = req.query.apellido */
+
+    res.json({
+        saludo: `Hola soy ${nombre} ${apellido}`,
+    });
+});
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en http://localhost:3000")
